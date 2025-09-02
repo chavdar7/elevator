c356eccf78a1ea618a99f989aa8cc4ffb0243c5d
chavdar7 <furkanchavdar@gmail.com>
2025-09-02 19:48:37 +0300

    app dosyasına api sayfaları eklendi

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template, jsonify, request
 from flask_socketio import SocketIO, emit
 import json
+from elevator_logic import AsansorSistemi
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asansor_secret_key'
 socketio = SocketIO(app)
 
+#sistemi burda oluşturcaz (instance)
+sistem = AsansorSistemi()
+
 #test
 @app.route('/')
-
 def ana_sayfa():
     return render_template('index.html')
 
 @app.route('/test')
-
 def test():
     return jsonify({
         'message' : 'Flask başarılı',
@@ -21,6 +23,16 @@ def test():
         'kat_sayisi' : 16
     })
 
+#sistem durum APIsi
+@app.route('/api/durum')
+def sistem_durumu():
+    return jsonify(sistem.sistem_durumu())
+
+#çağrı yapma APIsi
+@app.route('/api/cagri/<int:kat>/<yon>')
+def cagri_yap(kat, yon):
+    sonuc = sistem.cagri_yap(kat, yon)
+    return jsonify(sonuc)
 
 
 if __name__ == '__main__':
